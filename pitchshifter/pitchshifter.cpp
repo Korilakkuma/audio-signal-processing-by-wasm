3d51e2e25ae2fa830ff68a6b52a406a2b129d03b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-10-20 23:58:32 +0900

    feat: Pitch Shifter by C++ (without JavaScript that Emscripten generates)

--- pitchshifter/pitchshifter.cpp
+++ pitchshifter/pitchshifter.cpp
@@ -1,14 +1,20 @@
+#include <stdlib.h>
 #include <math.h>
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 
-inline int pow2(int n);
-inline void swap(float *real, float *imag, int i, int k);
+static float *inputLs  = NULL;
+static float *inputRs  = NULL;
+static float *outputLs = NULL;
+static float *outputRs = NULL;
 
-void FFT(float *real, float *imag, int N);
-void IFFT(float *real, float *imag, int N);
+static inline int pow2(int n);
+static inline void swap(float *reals, float *imags, int i, int k);
+
+static void FFT(float *reals, float *imags, int size);
+static void IFFT(float *reals, float *imags, int size);
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,15 +23,33 @@ extern "C" {
 #ifdef __EMSCRIPTEN__
 EMSCRIPTEN_KEEPALIVE
 #endif
-void pitchshifter(double pitch, float *input_reals, float *input_imags, float *output_reals, float *output_imags, int buffer_size) {
+float *pitchshifterL(const float pitch, const int buffer_size) {
+  if (outputLs) {
+    free(outputLs);
+  }
+
+  outputLs = (float *)calloc(buffer_size, sizeof(float));
+
+  float *input_reals  = (float *)calloc(buffer_size, sizeof(float));
+  float *input_imags  = (float *)calloc(buffer_size, sizeof(float));
+  float *output_reals = (float *)calloc(buffer_size, sizeof(float));
+  float *output_imags = (float *)calloc(buffer_size, sizeof(float));
+
+  for (int n = 0; n < buffer_size; n++) {
+    input_reals[n] = inputLs[n];
+    input_imags[n] = 0.0f;
+  }
+
   FFT(input_reals, input_imags, buffer_size);
 
+  int half_buffer_size = buffer_size / 2;
+
   for (int k = 0; k < buffer_size; k++) {
-    int offset = floor(pitch * k);
+    int offset = (int)floorf(pitch * k);
 
     int eq = 1;
 
-    if (k > (buffer_size / 2)) {
+    if (k > half_buffer_size) {
       eq = 0;
     }
 
@@ -36,13 +60,103 @@ void pitchshifter(double pitch, float *input_reals, float *input_imags, float *o
   }
 
   IFFT(output_reals, output_imags, buffer_size);
+
+  for (int n = 0; n < buffer_size; n++) {
+    outputLs[n] = output_reals[n];
+  }
+
+  free(input_reals);
+  free(input_imags);
+  free(output_reals);
+  free(output_imags);
+
+  return outputLs;
+}
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+float *pitchshifterR(const float pitch, const int buffer_size) {
+  if (outputRs) {
+    free(outputRs);
+  }
+
+  outputRs = (float *)calloc(buffer_size, sizeof(float));
+
+  float *input_reals  = (float *)calloc(buffer_size, sizeof(float));
+  float *input_imags  = (float *)calloc(buffer_size, sizeof(float));
+  float *output_reals = (float *)calloc(buffer_size, sizeof(float));
+  float *output_imags = (float *)calloc(buffer_size, sizeof(float));
+
+  for (int n = 0; n < buffer_size; n++) {
+    input_reals[n] = inputRs[n];
+    input_imags[n] = 0.0f;
+  }
+
+  FFT(input_reals, input_imags, buffer_size);
+
+  int half_buffer_size = buffer_size / 2;
+
+  for (int k = 0; k < buffer_size; k++) {
+    int offset = (int)floorf(pitch * k);
+
+    int eq = 1;
+
+    if (k > half_buffer_size) {
+      eq = 0;
+    }
+
+    if ((offset >= 0) && (offset < buffer_size)) {
+      output_reals[offset] += eq * input_reals[k];
+      output_imags[offset] += eq * input_imags[k];
+    }
+  }
+
+  IFFT(output_reals, output_imags, buffer_size);
+
+  for (int n = 0; n < buffer_size; n++) {
+    outputRs[n] = output_reals[n];
+  }
+
+  free(input_reals);
+  free(input_imags);
+  free(output_reals);
+  free(output_imags);
+
+  return outputRs;
+}
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+float *alloc_memory_inputLs(const int buffer_size) {
+  if (inputLs) {
+    free(inputLs);
+  }
+
+  inputLs = (float *)calloc(buffer_size, sizeof(float));
+
+  return inputLs;
+}
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+float *alloc_memory_inputRs(const int buffer_size) {
+  if (inputRs) {
+    free(inputRs);
+  }
+
+  inputRs = (float *)calloc(buffer_size, sizeof(float));
+
+  return inputRs;
 }
 
 #ifdef __cplusplus
 }
 #endif
 
-inline int pow2(int n) {
+static inline int pow2(int n) {
   if (n == 0) {
     return 1;
   }
@@ -50,22 +164,22 @@ inline int pow2(int n) {
   return 2 << (n - 1);
 }
 
-inline void swap(float *real, float *imag, int i, int k) {
+static inline void swap(float *reals, float *imags, int i, int k) {
   float tmp_real;
   float tmp_imag;
 
-  tmp_real = real[i];
-  tmp_imag = imag[i];
+  tmp_real = reals[i];
+  tmp_imag = imags[i];
 
-  real[i] = real[k];
-  imag[i] = imag[k];
+  reals[i] = reals[k];
+  imags[i] = imags[k];
 
-  real[k] = tmp_real;
-  imag[k] = tmp_imag;
+  reals[k] = tmp_real;
+  imags[k] = tmp_imag;
 }
 
-void FFT(float *real, float *imag, int N) {
-  int number_of_stages = log2(N);
+static void FFT(float *reals, float *imags, int size) {
+  int number_of_stages = (int)log2((double)size);
 
   for (int stage = 1; stage <= number_of_stages; stage++) {
     for (int i = 0; i < pow2(stage - 1); i++) {
@@ -76,29 +190,29 @@ void FFT(float *real, float *imag, int N) {
         int m = pow2(rest) + n;
         int r = j * pow2(stage - 1);
 
-        float a_real = real[n];
-        float a_imag = imag[n];
-        float b_real = real[m];
-        float b_imag = imag[m];
-        float c_real = cos((2.0 * M_PI * r) / N);
-        float c_imag = 0 - sin((2.0 * M_PI * r) / N);
+        float a_real = reals[n];
+        float a_imag = imags[n];
+        float b_real = reals[m];
+        float b_imag = imags[m];
+        float c_real = cosf((2.0f * M_PI * r) / size);
+        float c_imag = 0 - sinf((2.0f * M_PI * r) / size);
 
         if (stage < number_of_stages) {
-          real[n] = a_real + b_real;
-          imag[n] = a_imag + b_imag;
-          real[m] = (c_real * (a_real - b_real)) - (c_imag * (a_imag - b_imag));
-          imag[m] = (c_real * (a_imag - b_imag)) + (c_imag * (a_real - b_real));
+          reals[n] = a_real + b_real;
+          imags[n] = a_imag + b_imag;
+          reals[m] = (c_real * (a_real - b_real)) - (c_imag * (a_imag - b_imag));
+          imags[m] = (c_real * (a_imag - b_imag)) + (c_imag * (a_real - b_real));
         } else {
-          real[n] = a_real + b_real;
-          imag[n] = a_imag + b_imag;
-          real[m] = a_real - b_real;
-          imag[m] = a_imag - b_imag;
+          reals[n] = a_real + b_real;
+          imags[n] = a_imag + b_imag;
+          reals[m] = a_real - b_real;
+          imags[m] = a_imag - b_imag;
         }
       }
     }
   }
 
-  int *index = (int *)calloc(N, sizeof(int));
+  int *index = (int *)calloc(size, sizeof(int));
 
   for (int stage = 1; stage <= number_of_stages; stage++) {
     int rest = number_of_stages - stage;
@@ -108,19 +222,19 @@ void FFT(float *real, float *imag, int N) {
     }
   }
 
-  for (int k = 0; k < N; k++) {
+  for (int k = 0; k < size; k++) {
     if (index[k] <= k) {
       continue;
     }
 
-    swap(real, imag, index[k], k);
+    swap(reals, imags, index[k], k);
   }
 
   free(index);
 }
 
-void IFFT(float *real, float *imag, int N) {
-  int number_of_stages = log2(N);
+static void IFFT(float *reals, float *imags, int size) {
+  int number_of_stages = (int)log2((double)size);
 
   for (int stage = 1; stage <= number_of_stages; stage++) {
     for (int i = 0; i < pow2(stage - 1); i++) {
@@ -131,29 +245,29 @@ void IFFT(float *real, float *imag, int N) {
         int m = pow2(rest) + n;
         int r = j * pow2(stage - 1);
 
-        float a_real = real[n];
-        float a_imag = imag[n];
-        float b_real = real[m];
-        float b_imag = imag[m];
-        float c_real = cos((2.0 * M_PI * r) / N);
-        float c_imag = sin((2.0 * M_PI * r) / N);
+        float a_real = reals[n];
+        float a_imag = imags[n];
+        float b_real = reals[m];
+        float b_imag = imags[m];
+        float c_real = cosf((2.0f * M_PI * r) / size);
+        float c_imag = sinf((2.0f * M_PI * r) / size);
 
         if (stage < number_of_stages) {
-          real[n] = a_real + b_real;
-          imag[n] = a_imag + b_imag;
-          real[m] = (c_real * (a_real - b_real)) - (c_imag * (a_imag - b_imag));
-          imag[m] = (c_real * (a_imag - b_imag)) + (c_imag * (a_real - b_real));
+          reals[n] = a_real + b_real;
+          imags[n] = a_imag + b_imag;
+          reals[m] = (c_real * (a_real - b_real)) - (c_imag * (a_imag - b_imag));
+          imags[m] = (c_real * (a_imag - b_imag)) + (c_imag * (a_real - b_real));
         } else {
-          real[n] = a_real + b_real;
-          imag[n] = a_imag + b_imag;
-          real[m] = a_real - b_real;
-          imag[m] = a_imag - b_imag;
+          reals[n] = a_real + b_real;
+          imags[n] = a_imag + b_imag;
+          reals[m] = a_real - b_real;
+          imags[m] = a_imag - b_imag;
         }
       }
     }
   }
 
-  int *index = (int *)calloc(N, sizeof(int));
+  int *index = (int *)calloc(size, sizeof(int));
 
   for (int stage = 1; stage <= number_of_stages; stage++) {
     int rest = number_of_stages - stage;
@@ -163,17 +277,17 @@ void IFFT(float *real, float *imag, int N) {
     }
   }
 
-  for (int k = 0; k < N; k++) {
+  for (int k = 0; k < size; k++) {
     if (index[k] <= k) {
       continue;
     }
 
-    swap(real, imag, index[k], k);
+    swap(reals, imags, index[k], k);
   }
 
-  for (int k = 0; k < N; k++) {
-    real[k] /= N;
-    imag[k] /= N;
+  for (int k = 0; k < size; k++) {
+    reals[k] /= size;
+    imags[k] /= size;
   }
 
   free(index);
