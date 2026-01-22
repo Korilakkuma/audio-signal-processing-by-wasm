68fc7c607406c92dd198e47c37310b5c15eac176
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2026-01-22 19:57:02 +0900

    feat: FFT (uses WebAssembly SIMD)

---
+++ SIMD-FFT/FFT.cpp
@@ -0,0 +1,278 @@
+#include <stdlib.h>
+#include <math.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <wasm_simd128.h>
+#endif
+
+typedef enum {
+  RECTANGULAR,
+  HANNING,
+  HAMMING
+} WINDOW_FUNCTION;
+
+
+static float *reals = nullptr;
+static float *imags = nullptr;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 2^{n}
+static inline int pow2(const int n) {
+  if (n == 0) {
+    return 1;
+  }
+
+  return 2 << (n - 1);
+}
+
+static inline void swap(float *const reals, float *const imags, const int i, const size_t k) {
+  float tmp_real;
+  float tmp_imag;
+
+  tmp_real = reals[i];
+  tmp_imag = imags[i];
+
+  reals[i] = reals[k];
+  imags[i] = imags[k];
+
+  reals[k] = tmp_real;
+  imags[k] = tmp_imag;
+}
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+void window_function(float *const window, const size_t size, const WINDOW_FUNCTION function) {
+  switch (function) {
+    case HANNING: {
+      for (int n = 0; n < size; n++) {
+        if (n & 0x00000001) {
+          window[n] = 0.5 - (0.5 * cosf(((2 * M_PI) * (n + 0.5)) / size));
+        } else {
+          window[n] = 0.5 - (0.5 * cosf(((2 * M_PI) * n) / size));
+        }
+      }
+
+      break;
+    }
+
+    case HAMMING: {
+      for (int n = 0; n < size; n++) {
+        if (n & 0x00000001) {
+          window[n] = 0.54 - (0.46 * cosf(((2 * M_PI) * (n + 0.5)) / size));
+        } else {
+          window[n] = 0.54 - (0.46 * cosf(((2 * M_PI) * n) / size));
+        }
+      }
+
+      break;
+    }
+
+    case RECTANGULAR: {
+      for (int n = 0; n < size; n++) {
+        window[n] = 1.0f;
+      }
+
+      break;
+    }
+  }
+}
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+void FFT(const size_t size) {
+  int number_of_stages = (int)log2f((float)size);
+
+#ifdef __WASM_SIMD128_H
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      int rest = number_of_stages - stage;
+
+      for (int j = 0; j < pow2(rest); j++) {
+        int n = i * pow2(rest + 1) + j;
+        int m = pow2(rest) + n;
+
+        float w = 2.0f * M_PI * j * pow2(stage - 1);
+
+        float e_real = reals[n];
+        float e_imag = imags[n];
+        float o_real = reals[m];
+        float o_imag = imags[m];
+        float w_real = cosf(w / size);
+        float w_imag = 0.0f - sinf(w / size);
+
+        v128_t v1 = wasm_f32x4_make(e_real, e_imag,  (w_real * (e_real - o_real)), (w_real * (e_imag - o_imag)));
+        v128_t v2 = wasm_f32x4_make(o_real, o_imag, -(w_imag * (e_imag - o_imag)), (w_imag * (e_real - o_real)));
+
+        if (stage < number_of_stages) {
+          v128_t v = wasm_f32x4_add(v1, v2);
+
+          reals[n] = wasm_f32x4_extract_lane(v, 0);
+          imags[n] = wasm_f32x4_extract_lane(v, 1);
+          reals[m] = wasm_f32x4_extract_lane(v, 2);
+          imags[m] = wasm_f32x4_extract_lane(v, 3);
+        } else {
+          v128_t v_add = wasm_f32x4_add(v1, v2);
+          v128_t v_sub = wasm_f32x4_sub(v1, v2);
+
+          reals[n] = wasm_f32x4_extract_lane(v_add, 0);
+          imags[n] = wasm_f32x4_extract_lane(v_add, 1);
+          reals[m] = wasm_f32x4_extract_lane(v_sub, 0);
+          imags[m] = wasm_f32x4_extract_lane(v_sub, 1);
+        }
+      }
+    }
+  }
+#else
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      int rest = number_of_stages - stage;
+
+      for (int j = 0; j < pow2(rest); j++) {
+        int n = i * pow2(rest + 1) + j;
+        int m = pow2(rest) + n;
+
+        float w = 2.0f * M_PI * j * pow2(stage - 1);
+
+        float e_real = reals[n];
+        float e_imag = imags[n];
+        float o_real = reals[m];
+        float o_imag = imags[m];
+        float w_real = cosf(w / size);
+        float w_imag = 0.0f - sinf(w / size);
+
+        if (stage < number_of_stages) {
+          reals[n] = e_real + o_real;
+          imags[n] = e_imag + o_imag;
+          reals[m] = (w_real * (e_real - o_real)) - (w_imag * (e_imag - o_imag));
+          imags[m] = (w_real * (e_imag - o_imag)) + (w_imag * (e_real - o_real));
+        } else {
+          reals[n] = e_real + o_real;
+          imags[n] = e_imag + o_imag;
+          reals[m] = e_real - o_real;
+          imags[m] = e_imag - o_imag;
+        }
+      }
+    }
+  }
+#endif
+
+  size_t *index = (size_t *)calloc(size, sizeof(size_t));
+
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    int rest = number_of_stages - stage;
+
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      index[(size_t)(pow2(stage - 1) + i)] = index[i] + (size_t)pow2(rest);
+    }
+  }
+
+  for (size_t k = 0; k < size; k++) {
+    if (index[k] <= k) {
+      continue;
+    }
+
+    swap(reals, imags, index[k], k);
+  }
+
+  free(index);
+}
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+void IFFT(const size_t size) {
+  int number_of_stages = (int)log2f((float)size);
+
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      int rest = number_of_stages - stage;
+
+      for (int j = 0; j < pow2(rest); j++) {
+        int n = i * pow2(rest + 1) + j;
+        int m = pow2(rest) + n;
+
+        float w = 2.0f * M_PI * j * pow2(stage - 1);
+
+        float e_real = reals[n];
+        float e_imag = imags[n];
+        float o_real = reals[m];
+        float o_imag = imags[m];
+        float w_real = cosf(w / size);
+        float w_imag = sinf(w / size);
+
+        if (stage < number_of_stages) {
+          reals[n] = e_real + o_real;
+          imags[n] = e_imag + o_imag;
+          reals[m] = (w_real * (e_real - o_real)) - (w_imag * (e_imag - o_imag));
+          imags[m] = (w_real * (e_imag - o_imag)) + (w_imag * (e_real - o_real));
+        } else {
+          reals[n] = e_real + o_real;
+          imags[n] = e_imag + o_imag;
+          reals[m] = e_real - o_real;
+          imags[m] = e_imag - o_imag;
+        }
+      }
+    }
+  }
+
+  size_t *index = (size_t *)calloc(size, sizeof(size_t));
+
+  for (int stage = 1; stage <= number_of_stages; stage++) {
+    int rest = number_of_stages - stage;
+
+    for (int i = 0; i < pow2(stage - 1); i++) {
+      index[(size_t)(pow2(stage - 1) + i)] = index[i] + (size_t)pow2(rest);
+    }
+  }
+
+  for (int k = 0; k < size; k++) {
+    if (index[k] <= k) {
+      continue;
+    }
+
+    swap(reals, imags, index[k], k);
+  }
+
+  for (size_t k = 0; k < size; k++) {
+    reals[k] /= size;
+    imags[k] /= size;
+  }
+
+  free(index);
+}
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+float *alloc_memory_reals(const size_t buffer_size) {
+  if (reals) {
+    free(reals);
+  }
+
+  reals = (float *)calloc(buffer_size, sizeof(float));
+
+  return reals;
+}
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+float *alloc_memory_imags(const size_t buffer_size) {
+  if (imags) {
+    free(imags);
+  }
+
+  imags = (float *)calloc(buffer_size, sizeof(float));
+
+  return imags;
+}
+
+#ifdef __cplusplus
+}
+#endif
